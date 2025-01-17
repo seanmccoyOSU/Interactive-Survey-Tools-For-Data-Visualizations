a0cf987ff15c3694319a083ebcb5875b052a08b7
Sean McCoy <mccoyse@oregonstate.edu>
2025-01-16 16:36:29 -0800

    pan visual

--- public/src/visualizer.js
+++ public/src/visualizer.js
@@ -1,24 +1,67 @@
 let visualElements
+const wrapper = document.getElementById("wrapper")
+const visualContainer = document.getElementById("visual-container")
 
-fetch("../hyper.svg")
-  .then(response => response.text())
-  .then(svgText => {
-    // Create a new SVG element
-    const parser = new DOMParser();
-    const svgDoc = parser.parseFromString(svgText, "image/svg+xml");
-    const svg = document.body.appendChild(svgDoc.documentElement);
+document.addEventListener("DOMContentLoaded", LoadSvg)
 
-    visualElements = document.getElementsByTagName("path")
+function LoadSvg() {
+    // load svg file
+    fetch("../hyper.svg")
+    .then(response => response.text())
+    .then(svgText => {
+        // Create SVG element
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(svgText, "image/svg+xml");
+        let svg = visualContainer.appendChild(svgDoc.documentElement);
 
-    console.log(visualElements)
+        // Make elements selectable
+        visualElements = document.getElementsByTagName("path")
+        console.log(visualElements)
+        console.log(visualElements.length)
 
-    console.log(visualElements.length)
+        for(let i = 0; i < visualElements.length; i++) {
+            console.log("add listener")
+            visualElements.item(i).addEventListener("click", ToggleSelected)
+        }
 
-    for(let i = 0; i < visualElements.length; i++) {
-        console.log("add listener")
-        visualElements.item(i).addEventListener("click", ToggleSelected)
-    }
-  })
+        // Things to do after loading
+        OnLoadSvg()
+    })
+}
+
+function OnLoadSvg() {
+    EnablePanning()
+}
+
+function EnablePanning() {
+    let isDragging = false
+    let startX, startY
+
+    wrapper.addEventListener("mousedown", evt => {
+        isDragging = true
+        startX = evt.clientX - visualContainer.offsetLeft
+        startY = evt.clientY - visualContainer.offsetTop
+        wrapper.style.cursor = "grabbing"
+    })
+
+    document.addEventListener("mousemove", evt => {
+        if (isDragging) {
+            evt.preventDefault()
+
+            const x = evt.clientX - startX
+            const y = evt.clientY - startY
+
+            visualContainer.style.left = x + "px"
+            visualContainer.style.top = y + "px"
+        }
+    })
+
+    document.addEventListener("mouseup", () => {
+        isDragging = false
+        wrapper.style.cursor = "grab"
+    })
+}
+
 
 function ToggleSelected(evt) {
     evt.currentTarget.classList.toggle("selected")
