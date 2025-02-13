d93186eb3f006deb4d74591d9e48069f2afba4f1
Sean McCoy <mccoyse@oregonstate.edu>
2025-02-12 21:37:09 -0800

    visualization database container

--- make_containers_NO_API.sh
+++ make_containers_NO_API.sh
@@ -7,3 +7,10 @@ docker run -d --name mysql-server-visual-survey -p 3306:3306 \
 	-e "MYSQL_USER=visualsurvey" \
 	-e "MYSQL_PASSWORD=password" \
 	mysql
+sleep 8
+docker run -d --name mysql-server-visualization -p 3308:3306 \
+	-e "MYSQL_RANDOM_ROOT_PASSWORD=yes" \
+	-e "MYSQL_DATABASE=visualization" \
+	-e "MYSQL_USER=visualization" \
+	-e "MYSQL_PASSWORD=password" \
+	mysql
