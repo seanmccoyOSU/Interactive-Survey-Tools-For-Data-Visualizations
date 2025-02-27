aa0faf64b96ea67b189ce4b3e9be6f9bd2af7c42
Sean McCoy <mccoyse@oregonstate.edu>
2025-02-27 12:42:50 -0800

    user logout tests

--- __tests__/main/api/users.js
+++ __tests__/main/api/users.js
@@ -16,6 +16,16 @@ jest.mock('../../../main/api/lib/sequelize', () => {
 const sequelize = require('../../../main/api/lib/sequelize')
 const { User } = require('../../../main/api/model/User')
 
+const TEST_USER = { name:"testUser", password:"testPassword" }
+
+async function registerAndLogin(credentials) {
+    const registerRes = await request(api).post('/users').send(credentials)
+    const myId = registerRes.body.id
+    const loginRes = await request(api).post('/users/login').send(credentials)
+    const { header } = loginRes 
+    return {id: myId, res: loginRes, header: header}  
+}
+
 beforeEach(async () => {
     await sequelize.sync({ force: true })
 })
@@ -26,8 +36,8 @@ afterAll(async () => {
 
 describe("POST /users - registers user", () => {
     test("creates a new user in database, sends 201 status code and id", async () => {
-        const res = await request(api).post('/users').send({ name:"testUser", password:"testPassword" })    
-        const newUser = await User.findOne({ where: {name:"testUser"} })
+        const res = await request(api).post('/users').send(TEST_USER)    
+        const newUser = await User.findOne({ where: {name:TEST_USER.name} })
         expect(newUser).toBeTruthy()
         expect(res.statusCode).toBe(201)
         expect(res.body).toHaveProperty('id')
@@ -42,8 +52,8 @@ describe("POST /users - registers user", () => {
 
 describe("POST /users/login - logs user in", () => {
     test("sends 200 status code", async () => {
-        await request(api).post('/users').send({ name:"testUser", password:"testPassword" })
-        const res = await request(api).post('/users/login').send({ name:"testUser", password:"testPassword" })    
+        await request(api).post('/users').send(TEST_USER)
+        const res = await request(api).post('/users/login').send(TEST_USER)    
         expect(res.statusCode).toBe(200)
     })
     
@@ -56,12 +66,10 @@ describe("POST /users/login - logs user in", () => {
 
 describe("GET /users - get user info", () => {
     test("sends name, id, and 200 status code", async () => {
-        await request(api).post('/users').send({ name:"testUser", password:"testPassword" })
-        const loginRes = await request(api).post('/users/login').send({ name:"testUser", password:"testPassword" })
-        const { header } = loginRes    
-        const res = await request(api).get('/users').set("Cookie", [...header["set-cookie"]])
+        const loginDetails = await registerAndLogin(TEST_USER)
+        const res = await request(api).get('/users').set("Cookie", [...loginDetails.header["set-cookie"]])
         expect(res.statusCode).toBe(200)
-        expect(res.body).toHaveProperty('name', "testUser")
+        expect(res.body).toHaveProperty('name', TEST_USER.name)
         expect(res.body).toHaveProperty('id')
     })
     
@@ -72,22 +80,27 @@ describe("GET /users - get user info", () => {
     })
 })
 
+describe("POST /users/logout - logs user out", () => {
+    test("sends 200 status code and revokes authentication", async () => {
+        const loginDetails = await registerAndLogin(TEST_USER)
+        const logoutRes = await request(api).post('/users/logout').set("Cookie", [...loginDetails.header["set-cookie"]])
+        const { header } = logoutRes
+        const getRes = await request(api).get('/users').set("Cookie", [...header["set-cookie"]])
+        expect(logoutRes.statusCode).toBe(200)
+        expect(getRes.statusCode).toBe(401)
+    })
+})
+
 describe("GET /users/{id}/visualizations - get visualizations of user", () => {
     test("sends array of visualizations and 200 status code", async () => {
-        const registerRes = await request(api).post('/users').send({ name:"testUser", password:"testPassword" })
-        const myId = registerRes.body.id
-        const loginRes = await request(api).post('/users/login').send({ name:"testUser", password:"testPassword" })
-        const { header } = loginRes    
-        const res = await request(api).get(`/users/${myId}/visualizations`).set("Cookie", [...header["set-cookie"]])  
+        const loginDetails = await registerAndLogin(TEST_USER)   
+        const res = await request(api).get(`/users/${loginDetails.id}/visualizations`).set("Cookie", [...loginDetails.header["set-cookie"]])  
         expect(res.statusCode).toBe(200)
     })
     
     test("sends 401 status code and error when logged in as incorrect user", async () => {
-        const registerRes = await request(api).post('/users').send({ name:"testUser", password:"testPassword" })
-        const myId = registerRes.body.id
-        const loginRes = await request(api).post('/users/login').send({ name:"testUser", password:"testPassword" })
-        const { header } = loginRes    
-        const res = await request(api).get(`/users/${myId+1}/visualizations`).set("Cookie", [...header["set-cookie"]])    
+        const loginDetails = await registerAndLogin(TEST_USER) 
+        const res = await request(api).get(`/users/${loginDetails.id+1}/visualizations`).set("Cookie", [...loginDetails.header["set-cookie"]])    
         expect(res.statusCode).toBe(401)
         expect(res.body).toHaveProperty('error')
     })
