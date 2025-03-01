08f0b3f5fa8da6392d2e074be5c80aab09024138
Sean McCoy <mccoyse@oregonstate.edu>
2025-03-01 00:23:14 -0800

    full connection tests

--- __tests__/connection.js
+++ __tests__/connection.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
-const sequelize = require('../../../main/api/lib/sequelize')
+const sequelizeMain = require('../main/api/lib/sequelize')
+const sequelizeVisual = require('../visualization/api/lib/sequelize')
 
 const axios = require('axios')
 
@@ -17,11 +18,62 @@ test("successful connection to main API server", async () => {
     expect(unsuccessfulConnection).toBeFalsy()
 })
 
+test("successful connection to main UI server", async () => {
+    let unsuccessfulConnection = false
+
+    try {
+        await axios.get(process.env.MAIN_UI_URL)
+    } catch (error) {
+        if (!error.response)
+            unsuccessfulConnection = true
+    }
+
+    expect(unsuccessfulConnection).toBeFalsy()
+})
+
 test("successful connection to main database", async () => {
     let unsuccessfulConnection = false
 
     try {
-        await sequelize.authenticate()
+        await sequelizeMain.authenticate()
+    } catch (error) {
+        unsuccessfulConnection = true
+    }
+
+    expect(unsuccessfulConnection).toBeFalsy()
+})
+
+test("successful connection to visualization engine API server", async () => {
+    let unsuccessfulConnection = false
+
+    try {
+        await axios.get(process.env.VISUAL_API_URL)
+    } catch (error) {
+        if (!error.response)
+            unsuccessfulConnection = true
+    }
+
+    expect(unsuccessfulConnection).toBeFalsy()
+})
+
+test("successful connection to visualization engine UI server", async () => {
+    let unsuccessfulConnection = false
+
+    try {
+        await axios.get(process.env.VISUAL_UI_URL)
+    } catch (error) {
+        if (!error.response)
+            unsuccessfulConnection = true
+    }
+
+    expect(unsuccessfulConnection).toBeFalsy()
+})
+
+test("successful connection to visualization engine database", async () => {
+    let unsuccessfulConnection = false
+
+    try {
+        await sequelizeVisual.authenticate()
     } catch (error) {
         unsuccessfulConnection = true
     }
