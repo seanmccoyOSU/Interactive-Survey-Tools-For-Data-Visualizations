a57954174520ae6076df7b48eeab0472550f71d6
kielynd <148160695+kielynd@users.noreply.github.com>
2025-03-14 00:23:03 +0000

    Updating SurveyDesign to be in line with previously set up tables

--- main/api/model/SurveyDesign.js
+++ main/api/model/SurveyDesign.js
@@ -1,29 +1,57 @@
 const { DataTypes } = require('sequelize')
 const sequelize = require('../lib/sequelize')
+const { Question } = require('./Question');
 
 const SurveyDesign = sequelize.define('surveyDesign', {
-  name: { type: DataTypes.STRING, allowNull: false, unique: false },
-  title: { type: DataTypes.STRING, allowNull: true, unique: false },
-  introText: { type: DataTypes.TEXT, allowNull: true, unique: false },
-  conclusionText: { type: DataTypes.TEXT, allowNull: true, unique: false },
-}, {
-  hooks: {
-    beforeCreate: (surveyDesign, options) => {
-      surveyDesign.title = surveyDesign.name
-    }
+  survey_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+
+  desc: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+
+  },
+  created_by: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  start_date: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  end_date: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
   }
+}, {
+  tableName: 'surveyDesign',
+  timestamps: false
 })
+
+SurveyDesign.hasMany(Question, { foreignKey: 'survey_id' })
+Question.belongsTo(SurveyDesign, { foreignKey: 'survey_id' })
 
 exports.SurveyDesign = SurveyDesign
 
-/*
- * Export an array containing the names of fields the client is allowed to set
- * on users.
- */
 exports.SurveyDesignClientFields = [
-  'userId',
-  'name',
   'title',
-  'introText',
-  'conclusionText'
+  'desc',
+  'start_date',
+  'end_date',
+  'is_active'
 ]
