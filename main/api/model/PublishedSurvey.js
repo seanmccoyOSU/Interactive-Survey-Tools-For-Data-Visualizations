3317e23f00e0fc12daaa34eae8b04937bab2414b
Sean McCoy <mccoyse@oregonstate.edu>
2025-03-17 01:47:39 -0700

    fixed how link hash is generated

--- main/api/model/PublishedSurvey.js
+++ main/api/model/PublishedSurvey.js
@@ -6,26 +6,29 @@ const PublishedSurvey = sequelize.define('publishedSurvey', {
   name: { type: DataTypes.STRING, allowNull: false, unique: false },
   openDateTime: { type: DataTypes.DATE, allowNull: true, unique: false },
   closeDateTime: { type: DataTypes.DATE, allowNull: true, unique: false },
-  status: { type: DataTypes.STRING, allowNull: true, unique: false },
-  linkHash: { type: DataTypes.STRING, allowNull: true, unique: false },
+  status: {type: DataTypes.STRING, allowNull: true, unique: false},
+  linkHash: {type: DataTypes.STRING, allowNull: true, unique: false},
   surveyDesign: { type: DataTypes.JSON, allowNull: false, unique: false },
   questions: { type: DataTypes.JSON, allowNull: false, unique: false },
   results: { type: DataTypes.JSON, allowNull: true, unique: false }
 }, {
   hooks: {
-    beforeCreate: (publishedSurvey, options) => {
+    afterCreate: async (publishedSurvey, options) => {
+      
+      let updatedStatus
       if (Date.now() < publishedSurvey.openDateTime.valueOf()) {
-        publishedSurvey.status = "pending"
+        updatedStatus = "pending"
       } else if (Date.now() < publishedSurvey.closeDateTime.valueOf()) {
-        publishedSurvey.status = "in-progress"
+        updatedStatus = "in-progress"
       } else if (Date.now() < publishedSurvey.closeDateTime.valueOf()) {
-        publishedSurvey.status = "closed"
+        updatedStatus = "closed"
       }
 
-      const hash = crypto.createHash('sha1').update(String(publishedSurvey.id)).digest('hex')
-      console.log("HASH", hash)
-      console.log("HASH", hash.substring(0, 12))
-      publishedSurvey.linkHash = hash.substring(0, 12)
+      const hash = crypto.createHash('sha256').update(String(publishedSurvey.id)).digest('hex')
+
+      publishedSurvey.status = updatedStatus
+      publishedSurvey.linkHash = hash
+      await publishedSurvey.save()
     }
   }
 })
